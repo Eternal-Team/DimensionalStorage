a8c82da936714ffaf6fd8d3e67f469bbc262e4ea
Itorius <itorius.yt@gmail.com>
2019-07-06 16:23:36 +0200

    ControllerPanel

--- Components/Controller.cs
+++ Components/Controller.cs
@@ -1,10 +1,43 @@
-﻿namespace DimensionalStorage.Components
+﻿using BaseLibrary.UI;
+using System;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.ModLoader.IO;
+
+namespace DimensionalStorage.Components
 {
-	public class Controller : BaseComponent
+	public class Controller : BaseComponent, IHasUI
 	{
 		public override int DropItem => DimensionalStorage.Instance.ItemType<Items.Controller>();
 
 		public override int PortCapacity => 8;
 		public override int DriveCapacity => 4;
+
+		public Guid ID { get; set; }
+		public BaseUIPanel UI { get; set; }
+		public LegacySoundStyle CloseSound => SoundID.Item1;
+		public LegacySoundStyle OpenSound => SoundID.Item1;
+
+		public Controller()
+		{
+			ID = Guid.NewGuid();
+		}
+
+		public override bool Interact()
+		{
+			BaseLibrary.BaseLibrary.PanelGUI.UI.HandleUI(this);
+
+			return true;
+		}
+
+		public override TagCompound Save() => new TagCompound
+		{
+			["ID"] = ID.ToString()
+		};
+
+		public override void Load(TagCompound tag)
+		{
+			ID = tag.ContainsKey("ID") ? Guid.Parse(tag.GetString("ID")) : ID;
+		}
 	}
 }
---
+++ UI/ControllerPanel.cs
@@ -0,0 +1,34 @@
+﻿using BaseLibrary;
+using BaseLibrary.UI;
+using BaseLibrary.UI.Elements;
+using DimensionalStorage.Components;
+
+namespace DimensionalStorage.UI
+{
+	public class ControllerPanel : BaseUIPanel<Controller>
+	{
+		public override void OnInitialize()
+		{
+			Width = Height = (0, 0.25f);
+			this.Center();
+
+			UIText textLabel = new UIText("Controller")
+			{
+				HAlign = 0.5f
+			};
+			Append(textLabel);
+
+			UIText textDriveCapacity = new UIText("Drive Capacity: " + Container.Parent.Network.DriveCapacity)
+			{
+				Top = (28, 0)
+			};
+			Append(textDriveCapacity);
+
+			UIText textPortCapacity = new UIText("Port Capacity: " + Container.Parent.Network.PortCapacity)
+			{
+				Top = (56, 0)
+			};
+			Append(textPortCapacity);
+		}
+	}
+}
